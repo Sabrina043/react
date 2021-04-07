efee0628444e4be0f9e8096a94bc7b44f7688e6d
sabrina043 <slachehab6@gmail.com>
2021-04-07 15:45:19 +0200

    elsy

---
+++ chapitre_1/elsy/src/App.js
@@ -0,0 +1,23 @@
+const tempMin = -20
+const tempMax = 40
+const heartMin = 80
+const heartMax = 180
+const stepMin = 0
+const stepMax = 50000
+
+import React from "react";
+import './App.css';
+
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello !
+      </div>
+    );
+  }
+}
+
+export default App;

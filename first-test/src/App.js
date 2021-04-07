99fbf0c7a25e014c1270b023a1c25fbf7255b10a
sabrina043 <slachehab6@gmail.com>
2021-04-07 14:50:04 +0200

    react first-test

---
+++ first-test/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+
+  render() {
+
+    return (
+
+      <div >
+
+      <h1>sabrina</h1>
+      
+      </div>
+    );
+  }
+}
+
+export default App;

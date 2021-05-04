dd8cf230a2349eaa60a94a6c27cc84aba9c37990
sarah <slachehab6@gmail.com>
2021-05-04 17:29:27 +0200

    BrowserRouter

--- chapitre_6/catalog/src/App.js
+++ chapitre_6/catalog/src/App.js
@@ -1,28 +1,46 @@
 import './App.css';
 import React from 'react'
+import {BrowserRouter, Route, Link, Switch} from "react-router-dom"
+import Home from './component/Home' 
+import Contact from './component/Contact' 
+import About from './component/About' 
+import error_404 from './component/error_404'
+
+
 
 
 class App extends React.Component{
 
-  constructor(){
-    super()
+    render(){
+        return(
 
-  }
+            <div>
 
-  render(){
+                <BrowserRouter>
 
-    return(
+                    <nav>
 
-      <div>
+                        <ul>
+                            <li><link to = "/"/>Home</li>
+                            <li><link to = "/about"/>About</li>
+                            <li><link to = "/Contact"/>Contact</li>
 
-      </div>
+                        </ul>
+                    </nav>
+                    
 
-    )
+                </BrowserRouter>
+                
+            </div>
+        )
+    }
 
-  }
 
 
 } export default App
+
+
+
 
 
 

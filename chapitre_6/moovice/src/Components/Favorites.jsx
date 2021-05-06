e1d953689e412253bd90eef25aba1869e08dd23d
sarah <slachehab6@gmail.com>
2021-05-06 17:22:10 +0200

    exo6

--- chapitre_6/moovice/src/Components/Favorites.jsx
+++ chapitre_6/moovice/src/Components/Favorites.jsx
@@ -2,13 +2,30 @@ import React, { Component } from 'react';
 
 class Favorites extends Component {
 
+    getStorage() {
+        this.state = {
+
+            movies: [],
+            favIDs: localStorage.getItem("favIDs")
+
+        }
+    }
+
+
+    getMovie(id){
+
+        fetch("http://api.themoviedb.org/3/movie/{ID}?api_key={API_KEY}")
+
+    }
+
+
     render() {
 
         return (
 
             <div>
 
-                <h1>Je suis dans Favorite</h1>
+                <h1>Favorite</h1>
 
             </div>
         );

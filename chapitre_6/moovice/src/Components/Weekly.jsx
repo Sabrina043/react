fc2b181236aac41dc630d167db75dbfa8c11aa6b
sarah <slachehab6@gmail.com>
2021-05-25 15:04:59 +0200

    moovice_2_Bonus

--- chapitre_6/moovice/src/Components/Weekly.jsx
+++ chapitre_6/moovice/src/Components/Weekly.jsx
@@ -1,41 +1,41 @@
-// import React, { Component } from 'react';
-// import Card  from './Card'
-// import Moment from 'react-moment';
+import React, { Component } from 'react';
+import Card  from './Card'
+import Moment from 'react-moment';
 
-// class Weekly extends Component {
+class Weekly extends Component {
 
-//     state = {
-//         movies: []
-//     }
+    state = {
+        movies: []
+    }
 
-//     componentDidMount() {
-//         const url = `http://api.themoviedb.org/3/discover/movie?primary_release_date.gte={LAST_WEEK}&primary_release_date.lte=${TODAY}&api_key=${API_KEY}`
+    componentDidMount() {
+        const url = `http://api.themoviedb.org/3/discover/movie?primary_release_date.gte={LAST_WEEK}&primary_release_date.lte=${TODAY}&api_key=${API_KEY}`
 
-//         fetch(url)
-//             .then(response => response.json())
-//             .then(data => {
-//                 console.log("data in popular component did mount", data);
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                console.log("data in popular component did mount", data);
 
-//                 this.setState({
-//                     movies: data.results
-//                 })
-//             })
-//     }
+                this.setState({
+                    movies: data.results
+                })
+            })
+    }
 
-//     render() {
-//         return (
-//             <div>
-//                 <h1>Popular</h1>
+    render() {
+        return (
+            <div>
+                <h1>Popular</h1>
 
-//                 {
-//                     this.state.movies.map(elem => {
-//                         return <Card {...elem} />
-//                     })
-//                 }
+                {
+                    this.state.movies.map(elem => {
+                        return <Card {...elem} />
+                    })
+                }
 
-//             </div>
-//         );
-//     }
-// }
+            </div>
+        );
+    }
+}
 
-// export default Weekly;
+export default Weekly;
